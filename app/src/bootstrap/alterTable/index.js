4d8431dd11f800bfb6abb4820011baa1f7a742f2
Cyrus Ngan <cyrusncy@gmail.com>
2026-02-26 22:01:56 +0800

    ignore already exist error

--- app/src/bootstrap/alterTable/index.js
+++ app/src/bootstrap/alterTable/index.js
@@ -1,67 +1,77 @@
+const ignoreAlreadyExistsError = (error) => {
+  // Postgres codes: 42P07 (duplicate_table), 42710 (duplicate_object)
+  if (
+    error.code === '42P07' ||
+    error.code === '42710' ||
+    error.message.includes('already exists')
+  ) {
+    return
+  }
+  console.error(error)
+}
+
 module.exports = {
   async up({ strapi }) {
     const knex = await strapi.db.connection
 
-    await knex
-      .schema.alterTable('students', table => {
+    await knex.schema
+      .alterTable('students', (table) => {
         table.unique('regno')
       })
-      .catch(console.error)
+      .catch(ignoreAlreadyExistsError)
 
-    await knex.schema.alterTable('cohort_student_maps', table => {
-      table.unique(['regno', 'school_year'])
-      table.index(['classcode'])
-    })
-      .catch(console.error)
+    await knex.schema
+      .alterTable('cohort_student_maps', (table) => {
+        table.unique(['regno', 'school_year'])
+      })
+      .catch(ignoreAlreadyExistsError)
 
-    await knex.schema.alterTable('conducts', table => {
-      table.index(['item_code'])
-    })
-      .catch(console.error)
+    await knex.schema
+      .alterTable('cohort_student_maps', (table) => {
+        table.index(['classcode'])
+      })
+      .catch(ignoreAlreadyExistsError)
 
-    const tableNames = [
-      'attendances',
-      'conducts',
-    ]
+    await knex.schema
+      .alterTable('conducts', (table) => {
+        table.index(['item_code'])
+      })
+      .catch(ignoreAlreadyExistsError)
+
+    const tableNames = ['attendances', 'conducts']
 
     for (const tableName of tableNames) {
-      await knex.schema.alterTable(tableName, table => {
-        table.index(['regno', 'school_year', 'term'])
-      })
-        .catch(console.error)
+      await knex.schema
+        .alterTable(tableName, (table) => {
+          table.index(['regno', 'school_year', 'term'])
+        })
+        .catch(ignoreAlreadyExistsError)
     }
   },
   async down({ strapi }) {
     const knex = await strapi.db.connection
 
-    await knex
-      .schema.alterTable('students', table => {
-        table.dropUnique('regno')
+    await knex.schema
+      .alterTable('cohort_student_maps', (table) => {
+        table.dropUnique(['regno', 'school_year'])
+        table.dropIndex(['classcode'])
       })
-      .catch(console.error)
+      .catch(ignoreAlreadyExistsError)
 
-    await knex.schema.alterTable('cohort_student_maps', table => {
-      table.dropUnique(['regno', 'school_year'])
-      table.dropIndex(['classcode'])
-    })
-      .catch(console.error)
+    await knex.schema
+      .alterTable('conducts', (table) => {
+        table.dropIndex(['item_code'])
+      })
+      .catch(ignoreAlreadyExistsError)
 
-    await knex.schema.alterTable('conducts', table => {
-      table.dropIndex(['item_code'])
-
-    })
-      .catch(console.error)
-
-    const tableNames = [
-      'attendances',
-      'conducts',
-    ]
+    const tableNames = ['attendances', 'conducts']
 
     for (const tableName of tableNames) {
-      await knex.schema.alterTable(tableName, table => {
-        table.dropIndex(['regno', 'school_year', 'term'])
-      })
-        .catch(console.error)
+      await knex.schema
+        .alterTable(tableName, (table) => {
+          table.dropIndex(['regno', 'school_year', 'term'])
+        })
+        .catch(ignoreAlreadyExistsError)
     }
-  },
+  }
 }
